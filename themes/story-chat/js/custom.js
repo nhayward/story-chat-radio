4c2e45a4eba6dd51e7bf35020fa62e7bbdf57d72
GitSync <git-sync@trilby.media>
2019-10-14 21:32:20 -0400

    initial theming

---
+++ themes/story-chat/js/custom.js
@@ -0,0 +1,33 @@
+var isTouch = window.DocumentTouch && document instanceof DocumentTouch;
+
+function swapHeaderLogoOnScroll() {
+    // Has scrolled class on header
+    var zvalue = jQuery(document).scrollTop();
+    if ( zvalue > 75 ) {
+        jQuery("#header .logo img").attr("src", "/user/themes/story-chat/images/logo/StoryChatRadio_nav.png");
+        jQuery(".mobile-logo img").addClass("mobile-scrolled");
+    } else {
+        jQuery("#header .logo img").attr("src", "/user/themes/story-chat/images/logo/StoryChatRadio_nav_inverted.png");
+        jQuery(".mobile-logo img").removeClass("mobile-scrolled");
+    }
+}
+
+jQuery(document).ready(function() {
+
+    swapHeaderLogoOnScroll();
+
+    // Scroll Events
+    if (!isTouch){
+        jQuery(document).scroll(function() {
+            swapHeaderLogoOnScroll();
+        });
+    };
+
+    // Touch scroll
+    jQuery(document).on({
+        'touchmove': function(e) {
+            swapHeaderLogoOnScroll(); // Replace this with your code.
+        }
+    });
+
+});
